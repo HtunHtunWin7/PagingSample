16edca1c1a50ba40291b073b3c7daba19d57359d
HtunHtunWin7 <htunhtunwin.ttw7@gmail.com>
2021-01-10 21:32:22 +0630

    initial commit

---
+++ app/src/main/java/com/ttw/pagingsample/model/Movie.kt
@@ -0,0 +1,38 @@
+package com.ttw.pagingsample.model
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.Index
+import com.google.gson.annotations.SerializedName
+
+@Entity(
+    primaryKeys = ["id"]
+)
+data class Movie(
+    @field:SerializedName("id")
+    val id: Int,
+    @field:SerializedName("title")
+    val title: String,
+    @field:SerializedName("poster_path")
+    val poster_path: String?,
+    @field:SerializedName("vote_average")
+    val vote_average: String,
+    @field:SerializedName("overview")
+    val overview: String,
+    @field:SerializedName("release_date")
+    val release_date: String,
+    @field:SerializedName("popularity")
+    val popularity: Float,
+    @field:SerializedName("vote_count")
+    val vote_count: Int,
+    @field:SerializedName("video")
+    val video: Boolean,/*
+    @field:SerializedName("backdrop_path")
+    val backdrop_path: String,*/
+    @field:SerializedName("original_language")
+    val original_language: String,
+    @field:SerializedName("original_title")
+    val original_title: String,
+    @field:SerializedName("adult")
+    val adult: Boolean
+)
